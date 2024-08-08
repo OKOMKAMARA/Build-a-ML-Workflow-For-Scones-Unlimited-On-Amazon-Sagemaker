117ff6f0c038f4a3aad2e3374710f119f7b44d30
@okomkamara <108529222+OKOMKAMARA@users.noreply.github.com>
2024-08-08 17:58:05 +0100

    Build a ML Workflow For Scones On Amazon Sagemaker

---
+++ lowconfidence_filter.py
@@ -0,0 +1,29 @@
+import json
+
+
+THRESHOLD = .70
+
+
+def lambda_handler(event, context):
+  
+        # TODO: write code...
+    
+    # Grab the inferences from the event
+    inferences = event["body"]["inferences"]
+    
+    # Check if any values in our inferences are above THRESHOLD
+    meets_threshold = max(inferences)>THRESHOLD
+    
+    # If our threshold is met, pass our data back out of the
+    # Step Function, else, end the Step Function with an error
+    if meets_threshold:
+        pass
+    else:
+        raise("THRESHOLD_CONFIDENCE_NOT_MET")
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(event)
+    }
+    
+    
